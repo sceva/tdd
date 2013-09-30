fc8ceb5428ea1aa665dfd48c5362daab447d19c8
Steve Young <sy@foreignlanguageflashcards.com>
2013-09-30 17:47:13 -0500

    Added List model

--- lists/tests.py
+++ lists/tests.py
@@ -8,7 +8,7 @@ from django.test import Client, TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -29,30 +29,41 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.all().count(), 0)
 
         
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list = List()
+        list.save()
+        
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list
         second_item.save()
 
+        saved_lists = List.objects.all()
+        self.assertEqual(saved_lists.count(), 1)
+        self.assertEqual(saved_lists[0], list)
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list)
         
 class ListViewTest(TestCase):
 
     def test_list_view_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list)
+        Item.objects.create(text='itemey 2', list=list)
 
         client = Client()
         response = client.get('/lists/the-only-list-in-the-world/')
@@ -77,5 +88,4 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'A new list item'}
         )
-        self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
