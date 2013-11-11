d1ee507dc4b875162283ced02c9af77414da09cd
Steve Young <sy@foreignlanguageflashcards.com>
2013-11-11 15:39:08 -0600

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-"""
-
 from django.core.urlresolvers import resolve
 from django.test import Client, TestCase
 from django.http import HttpRequest
@@ -27,36 +22,7 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.all().count(), 0)
-
         
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list = List()
-        list.save()
-        
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list
-        second_item.save()
-
-        saved_lists = List.objects.all()
-        self.assertEqual(saved_lists.count(), 1)
-        self.assertEqual(saved_lists[0], list)
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list)
         
 class ListViewTest(TestCase):
 
