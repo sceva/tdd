a75385be6e435c74e204ecf6563046a921e520e1
Steve Young <sy@foreignlanguageflashcards.com>
2014-01-08 20:19:55 -0600

    modifying FTs to run on staging new files

---
+++ functional_tests/baseold.py
@@ -0,0 +1,45 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from .server_tools import reset_database
+import sys
+
+
+class FunctionalTest(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+            	cls.server_host = arg.split('=')[1]
+                cls.server_url = 'http://' + cls.server_host
+                cls.against_staging = True
+                return
+        LiveServerTestCase.setUpClass()
+        cls.against_staging = False
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if not self.against_staging:
+            LiveServerTestCase.tearDownClass()
+            
+    def setUp(self):
+        if self.against_staging:
+        	reset_database(self.server_host)
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        # centering does not work unless browser is at least 930 (I think)
+        #self.browser.maximize_window()
+        #self.browser.set_window_size(960,600)
+        
+    def tearDown(self):
+        self.browser.quit()
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
